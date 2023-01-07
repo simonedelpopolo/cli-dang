610a88a88c723d0a7e909d0d80ed136309f5e073
Simone Del Popolo <simonedelpopolo@outlook.com>
2023-01-07 20:16:54 +0000

    @cli-dang/input.options v2.0.1
    fixed and increased details for matching and throwing errors
    tests updated too

--- src/@cli-dang/input/lib/input/options.ts
+++ src/@cli-dang/input/lib/input/options.ts
@@ -1,35 +1,40 @@
 import match from './functions/match'
 import { error_message_text } from './functions/text/error_message'
-import { OftypesError } from 'oftypes'
+import { number_, OftypesError } from 'oftypes'
 
 export async function options( pattern:string, reference_to_flag:string ):Promise<OftypesError|object>{
 
-  /**
+  let parsed_options:object|undefined|OftypesError = undefined
+  if( await number_( pattern ) )
+    parsed_options = error_message_text( pattern, reference_to_flag )
+  else {
+    /**
    * @ flag options should match the pattern
    * [--flag=option1:value1|option2:value2]
    * | <-- the pipe symbol is mandatory when passing a group of options to a flag.
    *
    * @type {RegExp}
    */
-  const options_reg_expression = /(.*)[|](.*)/g
-  const matches = Array.from( pattern.matchAll( options_reg_expression ), matches => matches[ 0 ] )
+    const options_reg_expression = /(.*)[|](.*)/g
+    const matches = Array.from( pattern.matchAll( options_reg_expression ), matches => matches[ 0 ] )
 
+    let matches_single:RegExpMatchArray[]|boolean = false
 
-  let matches_single:RegExpMatchArray[]|boolean = false
-  let parsed_options:object|undefined|OftypesError = undefined
+    if (  matches.length === 0 ) {
 
-  if (  matches.length === 0 ) {
+      const options_single_reg_expression = /[^:]+/g
 
-    const options_single_reg_expression = /[^:]+/g
-    matches_single = Array.from( pattern.matchAll( options_single_reg_expression ), matches => matches )
-  
-    if ( matches_single.length > 1 || matches_single.length === 0 ) {
-      parsed_options = error_message_text( pattern, reference_to_flag )
+      matches_single = Array.from( pattern.matchAll( options_single_reg_expression ), matches => matches )
+
+      if ( matches_single.length > 2 || matches_single.length === 0 )
+        parsed_options = error_message_text( pattern, reference_to_flag )
+
     }
-  }
 
-  if( !parsed_options )
-    parsed_options = await match( matches[ 0 ] || pattern, !!matches_single ).catch( error => error )
+    if( !parsed_options )
+      parsed_options = await match( matches[ 0 ] || pattern, !!matches_single ).catch( error => error )
+
+  }
 
 
   return new Promise( ( resolve, reject ) => {
--- src/tests/cli-dang/input/options.test.ts
+++ src/tests/cli-dang/input/options.test.ts
@@ -1,7 +1,7 @@
-import { trace, trace_options } from '@cli-dang/activity'
 import * as tttt from 'trythistrythat'
 import { AssertionError } from 'assert'
 import { options } from '@cli-dang/input'
+import { trace, trace_options } from '@cli-dang/activity'
 
 export default async ( id ) => {
   let success = true
@@ -25,7 +25,7 @@ export default async ( id ) => {
     message = result.message
   }else {
     trace_options.mute = true
-    message = <string> await trace(actual).catch(error=>error.message)
+    message = <string> await trace( actual ).catch( error => error.message )
   }
 
   tttt.end( id, success, UNITName, message )
@@ -67,7 +67,7 @@ export async function options_match_single( id ){
   const result:boolean|AssertionError = await tttt.deepStrictEqual( async() => {
 
     actual = await options( 'file:', '--php-server-on' ).catch( error => error )
-    const expected = { file: '' }
+    const expected = { file: undefined }
 
     return tttt.resolvers( actual, expected )
   } )
@@ -78,35 +78,61 @@ export async function options_match_single( id ){
     message = result.message
   }else {
     trace_options.mute = true
-    message = <string> await trace(actual).catch(error=>error.message)
+    message = <string> await trace( actual ).catch( error => error.message )
   }
 
   tttt.end( id, success, UNITName, message )
 }
 
 export async function options_no_options_given( id ){
-  
+
   let success = true
   let message:undefined|string
   const UNITName = '@cli-dang/input.options rejects no options given'
   let actual:Error|undefined = undefined
-  
+
   const result:boolean|AssertionError = await tttt.deepStrictEqual( async() => {
-    
+
     actual = await options( '', '--php-server-on' ).catch( error => error )
     const expected = true
-    
+
     return tttt.resolvers( actual instanceof Error, expected )
   } )
-  
+
   if( result instanceof Error ){
     tttt.failed( UNITName )
     success = false
     message = result.message
   }else
     message = actual.message
-  
-  
+
+
+  tttt.end( id, success, UNITName, message )
+}
+
+export async function options_single_number_no_colon( id ){
+
+  let success = true
+  let message:undefined|string
+  const UNITName = '@cli-dang/input.options rejects when literal number are given\nnot followed by a colon'
+  let actual:Error|undefined = undefined
+
+  const result:boolean|AssertionError = await tttt.deepStrictEqual( async() => {
+
+    actual = await options( '3', '--php-server-on' ).catch( error => error )
+    const expected = true
+
+    return tttt.resolvers( actual instanceof Error, expected )
+  } )
+
+  if( result instanceof Error ){
+    tttt.failed( UNITName )
+    success = false
+    message = result.message
+  }else
+    message = actual.message
+
+
   tttt.end( id, success, UNITName, message )
 }
 
