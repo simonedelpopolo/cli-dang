47be78d342e3adb7af023f611414bcb1efbfcea7
Simone Del Popolo <simonedelpopolo@outlook.com>
2023-01-07 20:08:20 +0000

    @cli-dang/input v2.0.1
    removed method process_title from the exports

--- src/@cli-dang/input/index.ts
+++ src/@cli-dang/input/index.ts
@@ -1,4 +1,3 @@
 export { entry_point } from './lib/input/entry_point'
 export { options } from './lib/input/options'
-export { process_title } from './lib/input/process_title'
 export { processor } from './lib/input/processor'
