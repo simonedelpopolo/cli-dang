e52819a2d7f7c309d9ba7fc3f66e2e76209cd824
Simone Del Popolo <simonedelpopolo@outlook.com>
2023-01-07 21:18:55 +0000

    @cli-dang/input.Command v2.0.2
    fixup! export
    
    check files:
      - src/@cli-dang/shell/lib/input/dang_process.ts
      - src/@cli-dang/shell/dang.ts
    
    for an example of implementation.

--- src/@cli-dang/input/index.ts
+++ src/@cli-dang/input/index.ts
@@ -1,4 +1,4 @@
 export { entry_point } from './lib/input/entry_point'
 export { options } from './lib/input/options'
 export { processor } from './lib/input/processor'
-export { default as Commands } from './lib/input/command'
+export { default as Command } from './lib/input/command'
