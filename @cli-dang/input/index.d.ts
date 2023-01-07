b5c30d912dcbac81ea13d40184cac9a11eb25e96
Simone Del Popolo <simonedelpopolo@outlook.com>
2023-01-07 20:06:00 +0000

    @cli-dang/input v2.0.1
    fixed types

--- @cli-dang/input/index.d.ts
+++ @cli-dang/input/index.d.ts
@@ -1,7 +1,6 @@
 import { OftypesError } from 'oftypes'
-export declare type LogicParameter=( ( object: { [ p: string ]: unknown, keys: string[] } ) => Promise<object> );
-export declare type ParsedArgv = { [ p: string ]: unknown, keys: string[] };
-export declare function entry_point(argv: string[], logic: LogicParameter): Promise<object | OftypesError>;
+export declare type ParsedArgv = { [ p: string ]: unknown, keys: string[] }
+export declare type LogicParameter = ( data: ParsedArgv ) => Promise<void>
+export declare function entry_point(argv: string[], logic: LogicParameter): Promise<void>;
 export function options( pattern:string, reference_to_flag:string ):Promise<OftypesError|object>;
-export declare function process_title(parsed_argv: ParsedArgv, logic: LogicParameter, ejected?: undefined | string):Promise<object>;
 export declare function processor(argv: any): Promise<ParsedArgv>;
