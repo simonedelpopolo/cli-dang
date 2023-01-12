d8b7b61101ce77aa02507111e5d9c7c19e9dab25
Simone Del Popolo <simonedelpopolo@outlook.com>
2023-01-12 13:36:47 +0000

    @cli-dang
    updated fix-chmod.sh

--- shell_script/fix-chmod.sh
+++ shell_script/fix-chmod.sh
@@ -1,2 +1,4 @@
 chmod u+x ./tests/cli-dang/activity/processes/process.exit.test.js || true
 chmod u+x ./tests/cli-dang/activity/processes/process.trace.test.js || true
+chmod u+x ./tests/cli-dang/input/processes/intercept.process.js || true
+chmod u+x ./tests/cli-dang/input/processes/intercept.help.process.js || true
