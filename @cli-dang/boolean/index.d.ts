ee07f52d003ca211a54c06776ac7a24bb6c089cd
Simone Del Popolo <simonedelpopolo@outlook.com>
2023-01-12 13:33:04 +0000

    @cli-dang/boolean 2.0.2
    types and version

--- @cli-dang/boolean/index.d.ts
+++ @cli-dang/boolean/index.d.ts
@@ -1,4 +1,4 @@
-import { OftypesError } from 'oftypes';
-export declare function true_false(string: string): Promise<boolean | OftypesError>;
+import { OftypesError } from 'oftypes'
+export declare function true_false( string: string ): Promise<boolean | OftypesError>;
 export function map( logic:{[logic:string]: boolean}, against:string ): Promise<boolean|OftypesError>;
 
