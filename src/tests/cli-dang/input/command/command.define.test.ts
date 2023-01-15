f7c28f70efb7a4cc864a617cba9ff8b3bc883325
Simone Del Popolo <simonedelpopolo@outlook.com>
2023-01-15 22:58:02 +0000

    @cli-dang/input.Command.define v3.0.0
    UNIT test for the Command.define method

---
+++ src/tests/cli-dang/input/command/command.define.test.ts
@@ -0,0 +1,42 @@
+import * as assert from 'assert'
+import * as tttt from 'trythistrythat'
+import { Command } from '@cli-dang/input'
+
+export default async ( id ) => {
+
+  const UNITName = '@cli-dang/input.command.define help'
+
+  const command =  new Command()
+
+  command.define( 'init', <cb>( data, ...rest_args ) => {
+
+    let success = true
+    let message: undefined | string
+    let result:Error|undefined = undefined
+
+    try {
+      assert.deepStrictEqual( data.object[ '--bare' ], rest_args[ 0 ] )
+    } catch ( error ) {
+      result = error
+    }
+
+    if ( result instanceof Error ) {
+      tttt.failed( UNITName )
+      success = false
+      message = result.message
+    }
+
+    tttt.end( id, success, UNITName, message )
+  }, [ { data:'to_sanitise', } ] )
+
+  await command.flag( '--bare', { short:'--bare', type:'opts' } )
+  await command.intercept( {
+    object: { init: undefined, '--bare':{ data:'to_sanitise' }  },
+    keys: [
+      'init',
+      '--bare'
+    ],
+
+  } )
+
+}
